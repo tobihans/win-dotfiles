804472626ec43da553308795ee90220dc2f4c32e
Hans B. K. Tognon <git@tobihans.space>
2024-05-20 10:16:34 +0100

    latest updates

--- Documents/PowerShell/profile.ps1
+++ Documents/PowerShell/profile.ps1
@@ -1,6 +1,8 @@
+$env:EDITOR = "vim"
+
 # Starship Prompt
-$env:STARSHIP_CONFIG = "$HOME\Documents\PowerShell\config\config.toml"
-# Invoke-Expression (&starship init powershell)
+$env:STARSHIP_CONFIG = "$HOME\Documents\PowerShell\config\starship.toml"
+Invoke-Expression (&starship init powershell)
 
 # Helpers & Alias & Functions
 . $HOME\Documents\Powershell\functions.ps1
